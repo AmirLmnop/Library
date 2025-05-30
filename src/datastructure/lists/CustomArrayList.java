edf5b3648fa2e2b0161357980fed833a68af0bd1
Amir <m.amirho3ein.6@gmail.com>
2025-05-30 21:54:02 +0330

    adding lists classes

---
+++ src/datastructure/lists/CustomArrayList.java
@@ -0,0 +1,101 @@
+package datastructures.lists;
+import datastructures.interfaces.List;
+import java.util.Arrays;
+
+public class CustomArrayList<T> implements List<T> {
+    private static final int INITIAL_CAPACITY = 10;
+    private Object[] elements;
+    private int size = 0;
+
+    public CustomArrayList() {
+        elements = new Object[INITIAL_CAPACITY];
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        else {
+            return (T) elements[index];
+        }
+    }
+    @Override
+    public T set(int index, T element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        else {
+            T old = (T) elements[index];
+            elements[index] = element;
+            return old;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean add(Object o) {
+        if (o == null) {
+            throw new NullPointerException();
+        }
+        else{
+            ensureCapacity();
+            elements[size] = o;
+            size++;
+            return true;
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o == null) {
+            throw new NullPointerException();
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                if (elements[i].equals(o)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null) {
+            throw new NullPointerException();
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                if (elements[i].equals(o)) {
+                    System.arraycopy(elements, i+1, elements, i, size-i-1);
+                    elements[i] = null;
+                    size--;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
