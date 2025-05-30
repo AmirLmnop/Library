ec826dc62108079bdacb28d597bc5296f8c6ffbc
Amir <m.amirho3ein.6@gmail.com>
2025-05-30 21:55:25 +0330

    adding library classes

---
+++ src/library/Book.java
@@ -0,0 +1,53 @@
+package library;
+
+import datastructures.queues.CustomQueue;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+    private CustomQueue<String> waitlist;
+
+
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true;
+        this.waitlist = new CustomQueue<>();
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getIsbn() { return isbn; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+
+    public void addToWaitlist(Member member) {
+        waitlist.add(member.getMemberId());
+    }
+
+    public Member getNextInWaitlist() {
+        String memberId = waitlist.poll();
+        if (memberId == null) {
+            return null;
+        }
+        return new Member(memberId, " ");
+    }
+
+    public boolean hasWaitlist() {
+        return !waitlist.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", available=" + isAvailable +
+                '}';
+    }
+}
